8864177d23e406cdda255346e14becdf031d74aa
assim fayas <asim1fayas@gmail.com>
2023-06-20 11:20:27 +0530

    test

--- MyProjects/angularWorkouts/angularworkouts/src/app/app.module.ts
+++ MyProjects/angularWorkouts/angularworkouts/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule
   ],
   providers: [],
---
+++ MyProjects/angularWorkouts/angularworkouts/src/app/components/post/post.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+
+title:string='your post shows here'
+name:string='Anbin'
+massagepost='Massage post'
+}
